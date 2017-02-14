cb720203814a02ce31bac2301a35e45b5c8c2a3d
php 15 <php.15@metralha.cec.ime.usp.br>
2017-02-14 20:54:06 -0200

    Atualizando arquivos Views

---
+++ app/Http/Controllers/CursosController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+class CursosController extends Controller
+{
+
+  private $cursos = array(
+  'Lógica de programação', 'Desenvolvimento Web com PHP',
+  'Algoritmos e estrutura de dados', 'Test Driven Development');
+
+    public function index(){
+
+     $cursos = $this->cursos;
+     return view('cursos.index', compact('cursos'));
+     }
+
+     public function exibe($posicao){
+
+      $curso = $this->cursos[$posicao];
+      return view('cursos.exibe', compact('curso'));
+    }
+}
